40888d0dd30832baeb4201c88f5eba588735eebd
H. Cole Wiley <hcwiley@gmail.com>
2013-11-14 05:31:42 -0600

    changes to del and drive pow

--- blinder_bot/blinder_bot.ino
+++ blinder_bot/blinder_bot.ino
@@ -1,4 +1,5 @@
 int drivePow = 185;
+int del = 10;
 
 int pwm_a = 3;  //PWM control for motor outputs 1 and 2 is on digital pin 3
 int pwm_b = 11;  //PWM control for motor outputs 3 and 4 is on digital pin 11
@@ -19,38 +20,42 @@ void loop() {
   }
   char readIn = Serial.read();
   if(readIn == 'w'){
+    drivePow = 255;
     digitalWrite(dir_a, HIGH);
     digitalWrite(dir_b, HIGH);
     analogWrite(pwm_a, drivePow);
     analogWrite(pwm_b, drivePow);
-    delay(50);
+    delay(del);
     analogWrite(pwm_a, 0);
     analogWrite(pwm_b, 0);
   }
   else if( readIn == 's'){
+    drivePow = 225;
     digitalWrite(dir_a, LOW);
     digitalWrite(dir_b, LOW);
     analogWrite(pwm_a, drivePow);
     analogWrite(pwm_b, drivePow);
-    delay(50);
+    delay(del);
     analogWrite(pwm_a, 0);
     analogWrite(pwm_b, 0);
   }
   else if( readIn == 'a'){
-    digitalWrite(dir_a, LOW);
-    digitalWrite(dir_b, HIGH);
-    analogWrite(pwm_a, drivePow);
-    analogWrite(pwm_b, drivePow);
-    delay(50);
-    analogWrite(pwm_a, 0);
-    analogWrite(pwm_b, 0);
-  }
-  else if( readIn == 'd'){
+    drivePow = 185;
     digitalWrite(dir_a, HIGH);
     digitalWrite(dir_b, LOW);
     analogWrite(pwm_a, drivePow);
     analogWrite(pwm_b, drivePow);
-    delay(50);
+    delay(del);
+    analogWrite(pwm_a, 0);
+    analogWrite(pwm_b, 0);
+  }
+  else if( readIn == 'd'){
+    drivePow = 185;
+    digitalWrite(dir_a, LOW);
+    digitalWrite(dir_b, HIGH);
+    analogWrite(pwm_a, drivePow);
+    analogWrite(pwm_b, drivePow);
+    delay(del);
     analogWrite(pwm_a, 0);
     analogWrite(pwm_b, 0);
   }
